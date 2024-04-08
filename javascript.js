61ff8881491909040f06c07e59a41eb01b85cec7
LLXAlone <fjaguiluz@gmail.com>
2024-04-08 19:21:48 +0800

    Added playRound function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+const initialSelection = prompt("Rock, paper, or scissors?", "");
+const playerSelection = initialSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+
+
 function getComputerChoice() {
 
     let array = ["Rock", "Paper", "Scissors"];
@@ -5,4 +10,71 @@ function getComputerChoice() {
     return array[random];
 }   
 
-    
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection == "Rock") {
+        if (playerSelection === "rock") {
+            let match = "The computer chooses " + computerSelection + ". You tied!"
+            return match;
+        }
+
+        else if (playerSelection === "paper") {
+            let match = ("The computer chooses " + computerSelection + ". You win!")
+            return match;
+        }
+
+        else if (playerSelection === "scissors") {
+            let match = ("The computer chooses " + computerSelection + ". You lost!")
+            return match;
+        }
+
+        else {
+            let match = ("You didn't choose a proper answer!")
+            return match;
+        }
+    }
+
+    else if (computerSelection === "Paper"){
+        if (playerSelection === "rock") {
+            let match = "The computer chooses " + computerSelection + ". You lost!"
+            return match;
+        }
+
+        else if (playerSelection === "paper") {
+            let match = ("The computer chooses " + computerSelection + ". You tied!")
+            return match;
+        }
+
+        else if (playerSelection === "scissors") {
+            let match = ("The computer chooses " + computerSelection + ". You win!")
+            return match;
+        }
+
+        else {
+            let match = ("You didn't choose a proper answer!")
+            return match;
+        }
+    }
+
+    else if (computerSelection === "Scissors"){
+        if (playerSelection === "rock") {
+            let match = "The computer chooses " + computerSelection + ". You win!"
+            return match;
+        }
+
+        else if (playerSelection === "paper") {
+            let match = ("The computer chooses " + computerSelection + ". You lost!")
+            return match;
+        }
+
+        else if (playerSelection === "scissors") {
+            let match = ("The computer chooses " + computerSelection + ". You tied!")
+            return match;
+        }
+
+        else {
+            let match = ("You didn't choose a proper answer!")
+            return match;
+        }
+    }
+
+}
